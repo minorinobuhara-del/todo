abf95f583250521eca02a8c4d37590e14517a182
minori morisue <minori.nobuhara@gmail.com>
2025-11-24 15:33:26 +0900

    fillable

---
+++ src/app/Http/Controllers/TodoController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Todo;
+use App\Http\Requests\TodoRequest;
+
+class TodoController extends Controller
+{
+    public function index()
+    {
+        return view('index');
+    }
+}
